1d8702e53f38efa2cc755acfedbb3a504616995e
Martijn Gobes <martijn.gobes@gmail.com>
2022-12-01 15:21:40 +0100

    Day 1

--- d1.py
+++ d1.py
@@ -1,22 +1,7 @@
 
+def part1(line):
+    elves = sorted([sum([int(val) for val in section.split("\n") if val != ""]) for section in line.split("\n\n")])
 
-def part1(lines):
-    elves = []
+    print(elves[-1], sum(elves[-3:]))
 
-    buffer = 0
-    for line in lines:
-        if line.strip() == "":
-            elves.append(buffer)
-            buffer = 0
-        else:
-            buffer += int(line)
-    elves.append(buffer)
-
-    print(max(elves))
-
-    print(sum(sorted(elves)[-3:]))
-
-part1("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000".split("\n"))
-
-file = open("d1.txt").readlines()
-part1(file)
+part1(open("d1.txt").read())
