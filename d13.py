cf2b67d0d2cd3564ab69db95bc9065e465543455
Martijn Gobes <martijn.gobes@gmail.com>
2022-12-13 14:14:17 +0100

    D13 - cleanup

--- d13.py
+++ d13.py
@@ -1,4 +1,3 @@
-import ast
 import functools
 import itertools
 
@@ -6,47 +5,40 @@ import itertools
 def order(left, right):
     i = 0
     while i < len(left) and i < len(right):
-        if type(left[i]) == type(right[i]) == int:
-            if left[i] < right[i]:
-                return True
-            elif left[i] > right[i]:
-                return False
-            else:
+        if type(left[i]) is type(right[i]) is int:
+            if left[i] == right[i]:
                 i += 1
-        elif type(left[i]) == type(right[i]) == list:
+            else:
+                return left[i] < right[i]
+        elif type(left[i]) is type(right[i]) is list:
             cmp = order(left[i], right[i])
-            if cmp is not None: return cmp
-            if len(left[i]) < len(right[i]):
-                return True
-            elif len(left[i]) > len(right[i]):
-                return False
-            else:
+            if cmp is not None:
+                return cmp
+            if len(left[i]) == len(right[i]):
                 i += 1
+            else:
+                return len(left[i]) < len(right[i])
         else:
-            if type(left[i]) == int:
+            if type(left[i]) is int:
                 cmp = order([left[i]], right[i])
-                if cmp is not None: return cmp
             else:
                 cmp = order(left[i], [right[i]])
-                if cmp is not None: return cmp
+            if cmp is not None:
+                return cmp
             i += 1
 
-    if len(left) < len(right):
-        return True
-    elif len(left) > len(right):
-        return False
-    else:
+    if len(left) == len(right):
         return None
+    else:
+        return len(left) < len(right)
 
 
-pairs = [[ast.literal_eval(packet) for packet in p.split("\n")] for p in open("d13.txt").read().split("\n\n")]
+pairs = [[eval(packet) for packet in p.split("\n")] for p in open("d13.txt").read().split("\n\n")]
 
 count = sum([i + 1 for i, (left, right) in enumerate(pairs) if order(left, right)])
 
-pairs = list(itertools.chain(*pairs))
-pairs.extend([[[2]], [[6]]])
-
-pairs = sorted(pairs, key=functools.cmp_to_key(lambda x, y: -1 if order(x, y) else 1))
+pairs = sorted(list(itertools.chain(*pairs)) + [[[2]], [[6]]],
+               key=functools.cmp_to_key(lambda x, y: -1 if order(x, y) else 1))
 
 print(count)
 print((pairs.index([[2]]) + 1) * (pairs.index([[6]]) + 1))
