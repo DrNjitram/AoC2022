231ab4da6c1e523f84edcbf447ef3672ab788bda
Martijn Gobes <martijn.gobes@gmail.com>
2022-12-21 13:29:35 +0100

    D19 Done!

--- d19.py
+++ d19.py
@@ -13,7 +13,20 @@ def get_costs(cs):
 
 
 def add(l1, l2):
-    return [l1[i] + l2[i] for i in range(len(l1))]
+    return tuple([l1[i] + l2[i] for i in range(len(l1))])
+
+
+def subtract(l1, l2):
+    return tuple([l1[i] - l2[i] for i in range(len(l2))] + [l1[3]])
+
+
+def purchase_and_produce(stock: list, robots: list, purchase_index: int):
+    stock_ = add(stock, robots)
+    robots_ = list(robots)
+    if purchase_index >= 0:
+        robots_[purchase_index] += 1
+        stock_ = subtract(stock_, costs[purchase_index])
+    return stock_, tuple(robots_)
 
 
 def produce_time(robot, stock, time_left):
@@ -21,65 +34,60 @@ def produce_time(robot, stock, time_left):
     return res
 
 
-def intermediate(bp: Type[Blueprint], robots: list, stock: list, minute_max: int, minute: int,
-                 purchase: int, previous: list):
-    stock = add(stock, robots)
-    costs = [bp.ore, bp.clay, bp.obsidian, bp.geode]
-    if purchase > -1:
-        robots[purchase] += 1
-        for i in range(3):
-            stock[i] -= costs[purchase][i]
-
-    return iterate(bp, robots, stock, minute_max, minute, previous)
-
-
-def iterate(bp: Type[Blueprint], robots: list, stock: list, minute_max: int, minute: int,
-            previous: list):
+def iterate(bp: Type[Blueprint], robots=(1, 0, 0, 0), stock=(0, 0, 0, 0), time_left=24):
     global global_max
 
-    if stock[3] > global_max[0] and minute <= global_max[1]:
-        global_max = [stock[3], minute, global_max[2]]
+    if entry := (stock, robots, time_left) in seen:
+        return seen[entry]
 
-    if (a := (produce_time(robots[2], stock[2], minute_max - minute - 2) < bp.geode[2])) \
-            or (b := (minute == minute_max)) \
-            or (c := (stock[3] < global_max[0] and minute >= global_max[1])) \
-            or (d := (produce_time(robots[3], stock[3], minute_max - minute - 1) < global_max[2])):
-        # timeout
-        # behind best,
-        # cant ever produce another geode
-        # cant ever produce enough geodes
-        if a and stock[3] > 0:
-            stock[3] = stock[3] + robots[3] * (minute_max - minute)
-        global_max[2] = max(stock[3], global_max[2])
-        return global_max[2]
+    if time_left == 0:
+        global_max = max(stock[3], global_max)
+        seen[entry] = stock[3]
+        return stock[3]
 
-    costs = [bp.ore, bp.clay, bp.obsidian, bp.geode]
+    if produce_time(robots[3], stock[3], time_left) < global_max:
+        seen[entry] = 0
+        return 0
 
+    if produce_time(robots[2], stock[2], time_left - 2) < bp.geode[2]:
+        seen[entry] = stock[3] + robots[3] * time_left
+        return stock[3] + robots[3] * time_left
+
+    enough_robots = [robots[i] > ((max([rp[i] for rp in costs]) * time_left - stock[i]) // time_left) for i in
+                     range(3)] + [False]
     resources = [all([cost[i] <= stock[i] for i in range(len(cost))]) for cost in costs]
-    enough = [robots[i] > max([rp[i] for rp in costs]) for i in range(3)] + [False]
-    buyable = [resources[i] and not previous[i] and not enough[i] for i in range(4)]
 
-    if any(buyable):
-        if buyable[3]:
-            return intermediate(bp, robots[:], stock[:], minute_max, minute + 1, 3, buyable[:])
-        elif buyable[2] and robots[2] == 0:
-            return intermediate(bp, robots[:], stock[:], minute_max, minute + 1, 2, buyable[:])
-        elif buyable[1] and robots[1] == 0:
-            return intermediate(bp, robots[:], stock[:], minute_max, minute + 1, 1, buyable[:])
-        else:
-            min_range = -1 if buyable[1] and robots[1] == 0 else -2
-            return max(
-                [intermediate(bp, robots[:], stock[:], minute_max, minute + 1, i, buyable[:]) for i in
-                 range(3, min_range, -1) if
-                 (buyable[i] or i == -1)]
-            )
+    buyable = [resources[i] and not enough_robots[i] for i in range(4)]
+    buyable[1] = buyable[1] and not enough_robots[2]  # only need clay if we need obsidian
+
+    results = []  # results of this iteration
+    if buyable[3]:
+        stock_, robots_ = purchase_and_produce(stock, robots, 3)
+        results.append(iterate(bp, robots_, stock_, time_left - 1))
+    elif buyable[2]:
+        stock_, robots_ = purchase_and_produce(stock, robots, 2)
+        results.append(iterate(bp, robots_, stock_, time_left - 1))
     else:
-        stock = add(stock, robots)
+        if buyable[1]:
+            stock_, robots_ = purchase_and_produce(stock, robots, 1)
+            results.append(iterate(bp, robots_, stock_, time_left - 1))
+        if buyable[0]:
+            stock_, robots_ = purchase_and_produce(stock, robots, 0)
+            results.append(iterate(bp, robots_, stock_, time_left - 1))
+        if not buyable[0] or (stock[1] and not enough_robots[2] or stock[2]):
+            stock_, robots_ = purchase_and_produce(stock, robots, -1)
+            results.append(iterate(bp, robots_, stock_, time_left - 1))
 
-        return iterate(bp, robots[:], stock[:], minute_max, minute + 1, buyable[:])
+    score = max(results)
+    seen[entry] = score
+    return score
 
 
-for no, line in enumerate(open("d19_test.txt").readlines()[1:]):
+blueprints = []
+
+p1 = 0
+p2 = 1
+for no, line in enumerate(open("d19.txt").readlines()):
     blueprint = Blueprint
     line = line.strip().split(": ")[1]
     for section in line[:-1].split("."):
@@ -88,9 +96,20 @@ for no, line in enumerate(open("d19_test.txt").readlines()[1:]):
         vals = [(words[-1 - 3 * i], words[-2 - 3 * i]) for i in range(1 + ((len(words) - 6) // 3))]
         exec(f"blueprint.{t} = get_costs(vals)")
 
-    # Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
-    # Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+    global_max = 0
+    seen = {}
+    costs = [blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode]
+    geode = iterate(blueprint)
+    print(f"BP {no+1}: {(no+1)*geode}")
+    p1 += (no + 1) * geode
 
-    global_max = [0, 24, 0]  # geode at minute, total max
-    geode = iterate(blueprint, [1, 0, 0, 0], [0, 0, 0, 0], 24, 0, [False, False, False, False])
-    print(no, geode)
+    if no < 3:
+        global_max = 0
+        seen = {}
+        costs = [blueprint.ore, blueprint.clay, blueprint.obsidian, blueprint.geode]
+        geode = iterate(blueprint, time_left=32)
+        print(f"BP {no+1}: {geode}")
+        p2 *= geode
+
+print(p1)
+print(p2)
